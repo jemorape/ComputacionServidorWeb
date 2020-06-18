8261fe2a59301e306c50fe44cabff5b2f0b4b29e
emoraCR <emora@contractroom.com>
2020-06-18 11:22:04 -0500

    PaginaWeb
    Primer Tarea/Ejercicio para php

---
+++ tarea1/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Eduardo Mora</title>
+  <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+  <!-- Font Awesome icons (free version)-->
+  <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+  <!-- Google fonts-->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body id="page-top">
+  <!-- Navegación-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none">Eduardo Mora Pérez</span><span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/avataaars1.svg" alt="" /></span></a
+      ><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php">Materia</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="practica1.php">Práctica 1</a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container-fluid p-0">
+      <!--  Materia-->
+      <section class="resume-section">
+        <div class="resume-section-content">
+          <h1 class="mb-0">Computación en el <span class="text-primary">Servidor Web</span></h1>
+          <div class="subheading mb-5">Trabajos a desarrollar en la Materia</div>
+          <div class="subheading mb-5">Trabajo 1: Desarrollo web avanzado</div>
+          <p class="lead mb-5">En esta actividad se propone desarrollar una página web en PHP de temática libre.
+            El desarrollo se hará poniendo en práctica los conceptos estudiados en los dos primeros temas.
+            Por tanto, los requisitos mínimos serían los siguientes:
+            <ul class="fa-ul mb-0">
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Una estructura de control de cada tipo que hemos estudiado.
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Una función.
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Un array o matriz.
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Uso de alguna función de cadenas.
+                </li>
+            </ul>
+          </p>
+
+        </div>
+      </section>
+
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+  </body>
+  </html>
